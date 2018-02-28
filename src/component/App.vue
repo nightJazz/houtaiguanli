94ecada450165a0e4d5984f7907582fcd07e132c
夜幕暮夜 <a1664187734@163.com>
2018-02-28 09:56:43 +0800

    端口接收

--- src/component/App.vue
+++ src/component/App.vue
@@ -1,18 +1,21 @@
 <template>
-    <div>
-        {{val}}
-        <!-- 跟组件路由占位，通过url来控制页面展示 -->
-        <router-view></router-view>
-    </div>
+   <div>{{val}}</div>
 </template>
 
 <script>
+   
+
     export default {
         data () {
             return {
-                val:'vue脚手架测色'
+                val:'vue脚手架测色',
+
             }
+        },
+        created(){
+            this.$http.get(this.$api.logout).then(()=>console.log('登出'))
         }
+       
     }
 </script>
 
--- src/component/login/Login.vue
+++ src/component/login/Login.vue
@@ -1,13 +1,40 @@
 <template>
     <div>
-        登录
+        <el-menu :default-active="activeIndex2" class="el-menu-demo" mode="horizontal" @select="handleSelect" background-color="#666" text-color="#fff" active-text-color="#ffd04b">
+            <el-menu-item index="1">首页</el-menu-item>
+            <el-submenu index="2">
+                <template slot="title">个人中心</template>
+                <el-menu-item index="2-1">修改资料</el-menu-item>
+                <el-menu-item index="2-2">开通VIP</el-menu-item>
+                <el-menu-item index="2-3">更换账号</el-menu-item>
+                <!-- <el-submenu index="2-4">
+                    <template slot="title">选项4</template>
+                    <el-menu-item index="2-4-1">选项1</el-menu-item>
+                    <el-menu-item index="2-4-2">选项2</el-menu-item>
+                    <el-menu-item index="2-4-3">选项3</el-menu-item>
+                </el-submenu> -->
+            </el-submenu>
+            <el-menu-item index="3" >消息中心</el-menu-item>
+            <el-menu-item index="4">
+                <a href="/#/login" target="_blank">订单管理</a>
+            </el-menu-item>
+        </el-menu>
     </div>
 </template>
 
 <script>
-    export default {
-        
+export default {
+    data(){
+        return {
+            activeIndex2:'1'
+        }
+    },
+    methods:{
+        handleSelect(key,keyPath){
+            console.log(key,keyPath);
+        }
     }
+};
 </script>
 
 <style scoped>
